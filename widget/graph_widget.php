8ce6f8c0aa7e871c973e0c2633ba39607f2cc768
cagabi <cagabi@lapiluka.org>
2017-08-29 17:07:04 +0100

    Netbeans formatted

--- widget/graph_widget.php
+++ widget/graph_widget.php
@@ -1,15 +1,14 @@
 <?php
-    defined('EMONCMS_EXEC') or die('Restricted access');
+defined('EMONCMS_EXEC') or die('Restricted access');
 
-    global $session, $mysqli;
-    require_once "Modules/graph/graph_model.php";
-    $graph = new Graph($mysqli);
-    $savedgraphs = $graph->getall($session['userid']);
-    
-    $savedgraphsnamelist = array();
-    foreach ($savedgraphs as $savedgraph) {
-        $savedgraphsnamelist[] = array($savedgraph->id,$savedgraph->name);
-    }
-    
+global $session, $mysqli;
+require_once "Modules/graph/graph_model.php";
+$graph = new Graph($mysqli);
+$savedgraphs = $graph->getall($session['userid']);
+
+$savedgraphsnamelist = array();
+foreach ($savedgraphs as $savedgraph) {
+    $savedgraphsnamelist[] = array($savedgraph->id, $savedgraph->name);
+}
 ?>
 <script>var savedgraphsnamelist = <?php echo json_encode($savedgraphsnamelist); ?>;</script>
