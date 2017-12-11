818ae9402b4f5014bb8f1940926162880c303a68
TrystanLea <glyn.hudson@openenergymonitor.org>
2017-12-11 21:38:04 +0000

    fix bug when loading graph widget

--- widget/graph_widget.php
+++ widget/graph_widget.php
@@ -3,12 +3,13 @@ defined('EMONCMS_EXEC') or die('Restricted access');
 
 global $session, $mysqli;
 
-if (group_module_installed()) {
+// Check if group module is installed
+$group = false;
+$result = $mysqli->query("SHOW TABLES LIKE 'groups'");
+if ($result->num_rows > 0) {
     require_once "Modules/group/group_model.php";
     $group = new Group($mysqli, null, null, null, null);
 }
-else
-    $group = null;
 
 require_once "Modules/graph/graph_model.php";
 $graph = new Graph($mysqli, $group);
@@ -24,13 +25,5 @@ foreach ($savedgraphs['groups'] as $group_graphs) {
     }
 }
 
-function group_module_installed() {
-    global $mysqli;
-    $result = $mysqli->query("SHOW TABLES LIKE 'groups'");
-    if ($result->num_rows > 0)
-        return true;
-    else
-        false;
-}
 ?>
 <script>var savedgraphsnamelist = <?php echo json_encode($savedgraphsnamelist); ?>;</script>
