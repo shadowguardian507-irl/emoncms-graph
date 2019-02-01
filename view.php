4b06099ee14b2cd93330436752f5ab047b9e980b
Emrys <emrys@openenergymonitor.org>
2019-02-01 17:33:58 +0000

    made button an <a> tag

--- view.php
+++ view.php
@@ -174,8 +174,8 @@
             <div class="feed-options hide">
                 <div class="feed-options-header">
                     <div class="feed-options-title"><?php echo _('Feeds in view') ?></div>
-                    <div class="feed-options-show-options btn btn-default hide"><?php echo _('Show options') ?></div>
-                    <div class="feed-options-show-stats btn btn-default"><?php echo _('Show statistics') ?></div>
+                    <a href="#" class="feed-options-show-options btn btn-default hide"><?php echo _('Show options') ?></a>
+                    <a href="#" class="feed-options-show-stats btn btn-default"><?php echo _('Show statistics') ?></a>
                 </div>
 
                 
